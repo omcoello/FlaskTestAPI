856098ea51c2bd749da91f99f40e19ac60c7ba2c
omcoello <omcoello@espol.edu.ec>
2024-06-01 02:37:51 -0500

    Modify data management to handle json format instead of form

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from .config import Config
 from .database import SQLAlchemySingleton
 from .views import main, products_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
+    CORS(app)
     db = SQLAlchemySingleton.get_instance(app)
 
     app.register_blueprint(main)
--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from .models import Product
 from .database import SQLAlchemySingleton
 import json
 from .serializer import serialize_model_instance
+from sqlalchemy.exc import IntegrityError
 
 db = SQLAlchemySingleton.get_instance()
 
@@ -24,15 +25,20 @@ def list_products():
 
 @products_bp.route('/create', methods=['GET', 'POST'])
 def create_product():
-    code = request.form['code'] 
-    description = request.form['description']
-    status = request.form['status']
-    expireDate = request.form['expireDate']
-    product = Product(code=code, description=description, status=status, expireDate=expireDate)        
-    db.session.add(product)
-    db.session.commit()        
-    cleaned_product = serialize_model_instance(product)
-    return cleaned_product
+    try:
+        code = request.json['code'] 
+        description = request.json['description']
+        status = request.json['status']
+        expireDate = request.json['expireDate']
+        product = Product(code=code, description=description, status=status, expireDate=expireDate)        
+        db.session.add(product)
+        db.session.commit()        
+        cleaned_product = serialize_model_instance(product)
+        return cleaned_product
+    except IntegrityError as e:
+        db.session.rollback()  # Revertir la transacción
+        error_message = f"Error: Product with code '{code}' already exists."
+        return {'error': error_message}, 400, {'Content-Type': 'application/json'}
 
 @products_bp.route('/<code>')
 def view_product(code):
@@ -40,18 +46,18 @@ def view_product(code):
     cleaned_product = serialize_model_instance(product)
     return cleaned_product
 
-@products_bp.route('/<code>/update', methods=['GET', 'POST'])
+@products_bp.route('/<code>/update', methods=['PUT', 'POST'])
 def update_product(code):
     product = Product.query.filter_by(code=code).first_or_404()
-    product.description = request.form['description']
-    product.status = request.form['status']
+    product.description = request.json['description']
+    product.status = request.json['status']
 
-    product.expireDate = request.form['expireDate']
+    product.expireDate = request.json['expireDate']
     db.session.commit()
     cleaned_product = serialize_model_instance(product)
     return cleaned_product
 
-@products_bp.route('/<code>/delete', methods=['POST'])
+@products_bp.route('/<code>/delete', methods=['DELETE'])
 def delete_product(code):
     product = Product.query.filter_by(code=code).first_or_404()
     db.session.delete(product)
